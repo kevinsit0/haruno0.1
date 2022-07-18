18231c709f669665f1c672704776beb30b32f56c
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2022-07-17 20:07:52 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -15,7 +15,7 @@ X-WA-BIZ-DESCRIPTION:${biz.description.replace(/\n/g, '\\n')}
 END:VCARD
     `.trim()
     let kont = await conn.sendMessage(m.chat, { contacts: { displayName: 'Fadli', contacts: [{vcard}]}}, { quoted: m})
-    conn.reply(m.chat, 'Nomor owner itu bukan bot, tidak usah chat command bot.\nChat yang sopan kalau belum dibales ya jangan spam. Makasih', kont)
+    conn.reply(m.chat, )
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
