97527022e9acc1b66d344cef20b32fd3ebf7939e
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2022-07-17 20:40:38 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,10 +1,25 @@
-let handler = function (m) {
-  // this.sendContact(m.chat, '593 96 416 3489', '🐉Kevin💎', m)
-  this.sendContact(m.chat, '593964163489', '🐉Kevin💎', m)
+import PhoneNumber from 'awesome-phonenumber'
+let handler = async(m, { conn }) => {
+    let nomor = owner[0][0]
+    let number = nomor + '@s.whatsapp.net'
+    let biz = await conn.getBusinessProfile(number)
+
+    let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;Fadli;;;
+FN:Fadli
+TEL;type=CELL;type=VOICE;waid=${nomor}:${PhoneNumber('+' + nomor).getNumber('international')}
+X-WA-BIZ-NAME:Fadli
+X-WA-BIZ-DESCRIPTION:${biz.description.replace(/\n/g, '\\n')}
+END:VCARD
+    `.trim()
+    let kont = await conn.sendMessage(m.chat, { contacts: { displayName: 'Fadli', contacts: [{vcard}]}}, { quoted: m})
+    conn.reply(m.chat, 'Nomor owner itu bukan bot, tidak usah chat command bot.\nChat yang sopan kalau belum dibales ya jangan spam. Makasih', kont)
 }
-handler.help = ['owner', 'creador']
+handler.help = ['owner', 'creator']
 handler.tags = ['info']
 
-handler.command = /^(owner|creador)$/i
+handler.command = /^(owner|creator)$/i
 
-module.exports = handler
+export default handler
